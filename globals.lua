48afa9cdf331b0f5da43b5b1f3f0a7013bf3fa5d
Sam Deane <sam@elegantchaos.com>
2022-09-04 17:35:43 +0100

    moved globals into separate file

---
+++ globals.lua
@@ -0,0 +1,48 @@
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+--  Created by Samedi on 04/09/2022.
+--  All code (c) 2022, The Samedi Corporation.
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+function printf(format, ...)
+    logMessage(string.format(tostring(format), ...))
+end
+
+debugf = printf
+
+
+toolkit.Object = { className = "object" }
+
+function toolkit.Object:init(...)
+  return self
+end
+
+function toolkit.define(name, baseName)
+    local base
+    if baseName then
+        base = toolkit[baseName]
+        if not base then
+            printf("forward declared %s", baseName)
+            base = {}
+            toolkit[baseName] = base
+        end
+    else
+        base = toolkit.Object
+    end
+  
+  local definition = toolkit[name] or {}
+  setmetatable(definition, { __index = base })
+  definition.super = base
+  definition.className = name
+  toolkit[name] = definition
+  printf("declared %s", name)
+
+  function definition.new(...)
+    local instance = {}
+    setmetatable(instance, { __index = definition })
+    instance = instance:init(...)
+    return instance
+  end
+
+  return definition
+end
+
