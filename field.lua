89c83db35ab435077befa4121a75c0448e470ff7
Sam Deane <sam@elegantchaos.com>
2022-09-05 21:14:23 +0100

    added multiline text

---
+++ field.lua
@@ -0,0 +1,63 @@
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+--  Created by Samedi on 05/09/2022.
+--  All code (c) 2022, The Samedi Corporation.
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+local Field = toolkit.define('Field', 'Widget')
+
+function Field:init(rect, state, font)
+    state = state or {}
+    state.lines = state.lines or {}
+    state.scroll = state.scroll or 0
+    state.scrollToEnd = state.scrollToEnd or false
+
+    self.super.init(self, rect)
+    self.state = state
+    self.font = font
+    return self
+end
+
+function Field:hitTest(cursor)
+    return false
+end
+
+function Field:drawInLayer(layer)
+    local font = self.font or layer.defaultFont
+    local x = self.rect.x
+    local y = self.rect.y
+    local width = self.rect.width
+    local height = self.rect.height
+    local maxY = y + height
+    local state = self.state
+    local lines = state.lines
+    local scroll = state.scroll
+    local lineCount = math.floor(height / font.size)
+    if state.scrollToEnd then
+        scroll = #lines - lineCount
+        state.scroll = scroll
+        printf("scroll to end %s", scroll)
+    end
+
+    for i,line in ipairs(lines) do
+        printf("%s", i)
+        if i >= scroll then
+            y = y + font.size
+            addText(layer.layer, font.font, line, x, y)
+            if y > maxY then
+                break
+            end
+        end
+        i = i + 1
+    end 
+end
+
+function Field:append(text, scrollToEnd)
+    table.insert(self.state.lines, text)
+    self.state.scrollToEnd = scrollToEnd
+end
+
+function toolkit.Widget:addField(...)
+    local field = Field.new(...)
+    self:addWidget(field)
+    return field
+end
